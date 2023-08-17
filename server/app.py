45bfecb64b32ea31c60fddaf82c99a364cd23a53
betalantz <betalantz@gmail.com>
2023-08-17 10:00:13 -0700

    working solution for core deliverables

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, make_response, request
+from datetime import datetime
+
+from flask import Flask, abort, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-from models import Park, db
+from models import Campsite, Park, db
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -16,7 +18,79 @@ api = Api(app)
 
 
 class Parks(Resource):
-    pass
+    def get(self):
+        parks = [park.to_dict() for park in Park.query.all()]
+        if not len(parks):
+            abort(404, description="No parks found")
+        return make_response(parks, 200)
+
+    def post(self):
+        request_data = request.get_json()
+        new_park = Park(
+            name=request_data["name"],
+            address=request_data["address"],
+            entrance_fee=request_data["entrance_fee"],
+            has_trails=request_data["has_trails"],
+            has_RV_cleanout=request_data["has_RV_cleanout"],
+            begin_camping_season=datetime.strptime(
+                request_data["begin_camping_season"], "%Y-%m-%d"
+            ),
+            end_camping_season=datetime.strptime(
+                request_data["end_camping_season"], "%Y-%m-%d"
+            ),
+        )
+        db.session.add(new_park)
+        db.session.commit()
+        return make_response(new_park.to_dict(), 201)
+
+
+class ParkById(Resource):
+    def get(self, id):
+        park = Park.query.get_or_404(id)
+        return make_response(park.to_dict(), 200)
+
+    def patch(self, id):
+        park = Park.query.get_or_404(id)
+        request_data = request.get_json()
+        for key, value in request_data.items():
+            setattr(park, key, value)
+        db.session.commit()
+        return make_response(park.to_dict(), 200)
+
+    def delete(self, id):
+        park = Park.query.get_or_404(id)
+        db.session.delete(park)
+        db.session.commit()
+        return make_response("", 204)
+
+
+class Campsites(Resource):
+    def post(self):
+        request_data = request.get_json()
+        new_campsite = Campsite(**request_data)
+        db.session.add(new_campsite)
+        db.session.commit()
+        return make_response(new_campsite.to_dict(), 201)
+
+
+class CampsiteById(Resource):
+    def get(self, id):
+        campsite = Campsite.query.get_or_404(id)
+        return make_response(campsite.to_dict(), 200)
+
+    def patch(self, id):
+        campsite = Campsite.query.get_or_404(id)
+        request_data = request.get_json()
+        for key, value in request_data.items():
+            setattr(campsite, key, value)
+        db.session.commit()
+        return make_response(campsite.to_dict(), 200)
+
+
+api.add_resource(Parks, "/parks")
+api.add_resource(ParkById, "/parks/<int:id>")
+api.add_resource(Campsites, "/campsites")
+api.add_resource(CampsiteById, "/campsites/<int:id>")
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -1,18 +1,73 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
+MIN_FEE = 13.99
+MAX_FEE = 25
+MAX_CAPACITY = 10
+SITE_TYPES = ("tent", "RV")
+
 
 class Park(db.Model, SerializerMixin):
-    pass
+    __tablename__ = "parks"
+
+    serialize_rules = ("-campsites.park", "-created_at", "-updated_at")
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    address = db.Column(db.String, nullable=False)
+    entrance_fee = db.Column(db.Float)
+    has_trails = db.Column(db.Boolean)
+    has_RV_cleanout = db.Column(db.Boolean)
+    begin_camping_season = db.Column(db.DateTime)
+    end_camping_season = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    campsites = db.relationship("Campsite", back_populates="park", cascade="delete")
+
+    @validates("entrance_fee")
+    def validate_entrance_fee(self, key, fee):
+        if not MIN_FEE <= fee <= MAX_FEE:
+            raise ValueError("Fee must be between 13.99 and 25 inclusive")
+        return fee
 
     def __repr__(self):
-        pass
+        return f"<Park ID: {self.id} Name: {self.name} >"
 
 
 class Campsite(db.Model, SerializerMixin):
-    pass
+    __tablename__ = "campsites"
+
+    serialize_rules = ("-park.campsites", "-created_at", "-updated_at")
+
+    id = db.Column(db.Integer, primary_key=True)
+    max_capacity = db.Column(db.Integer)
+    type = db.Column(db.String)
+    site_fee = db.Column(db.Float)
+    has_water = db.Column(db.Boolean)
+    has_bathroom = db.Column(db.Boolean)
+    has_grill = db.Column(db.Boolean)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    park_id = db.Column(db.Integer, db.ForeignKey("parks.id"))
+
+    park = db.relationship("Park", back_populates="campsites")
+
+    @validates("max_capacity")
+    def validate_max_capacity(self, key, capacity):
+        if capacity > MAX_CAPACITY:
+            raise ValueError("Maximum capacity is 10 people")
+        return capacity
+
+    @validates("type")
+    def validate_type(self, key, type):
+        if not type in SITE_TYPES:
+            raise ValueError("Type must be 'tent' or 'RV'")
+        return type
 
     def __repr__(self):
-        pass
+        return f"<Campsite Id: {self.id} Park: {self.park.name} Max Capacity: {self.max_capacity} >"
