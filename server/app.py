30bb35de541118b28407e6169682a9a46587bd60
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-20 19:38:52 -0700

    migrate models

--- server/app.py
+++ server/app.py
@@ -1,17 +1,21 @@
 from datetime import datetime
 
-from flask import Flask, abort, make_response, request
+from models import db, Park, Campsite, Reservation
 from flask_migrate import Migrate
+from flask import Flask, request
 from flask_restful import Api, Resource
-from models import Campsite, Park, db
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# Note: `app.json.compact = False` Configures JSON responses to print on indented lines
 app.json.compact = False
 
 migrate = Migrate(app, db)
+
 db.init_app(app)
 
 api = Api(app)
